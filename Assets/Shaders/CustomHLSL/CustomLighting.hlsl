0aeb0691c295eed40521313d75688f9dbec8ceaf
Andrew Delarge <cryenginemail@gmail.com>
2021-09-16 23:44:47 +0400

    shaders update

--- Assets/Shaders/CustomHLSL/CustomLighting.hlsl
+++ Assets/Shaders/CustomHLSL/CustomLighting.hlsl
@@ -4,6 +4,7 @@
 
 #if defined(SHADERGRAPH_PREVIEW)
 #else
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
 #pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
 #pragma multi_compile _ _ADDITIONAL_LIGHTS_VERTEX _ADDITIONAL_LIGHTS
@@ -40,36 +41,41 @@ void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, ou
 #endif
 }
 
+//
+// void DirectSpecular_float(float Smoothness, float3 Direction, float3 WorldNormal, float3 WorldView, out float3 Out)
+// {
+//     float4 White = 1;
+//
+// #if defined(SHADERGRAPH_PREVIEW)
+//     Out = 0;
+// #else
+//     Smoothness = exp2(10 * Smoothness + 1);
+//     WorldNormal = normalize(WorldNormal);
+//     WorldView = SafeNormalize(WorldView);
+//     Out = LightingSpecular(White, Direction, WorldNormal, WorldView, White, Smoothness);
+// #endif
+// }
 
-void DirectSpecular_float(float Smoothness, float3 Direction, float3 WorldNormal, float3 WorldView, out float3 Out)
-{
-    float4 White = 1;
 
-#if defined(SHADERGRAPH_PREVIEW)
-    Out = 0;
-#else
-    Smoothness = exp2(10 * Smoothness + 1);
-    WorldNormal = normalize(WorldNormal);
-    WorldView = SafeNormalize(WorldView);
-    Out = LightingSpecular(White, Direction, WorldNormal, WorldView, White, Smoothness);
-#endif
-}
-
-void AdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse, out float3 Specular)
+void AdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, out float3 Diffuse,
+    out float3 Specular)
 {
     float3 diffuseColor = 0;
     float3 specularColor = 0;
     float4 White = 1;
-
+    
 #if !defined(SHADERGRAPH_PREVIEW)
     Smoothness = exp2(10 * Smoothness + 1);
     WorldNormal = normalize(WorldNormal);
     WorldView = SafeNormalize(WorldView);
+    
+    
     int pixelLightCount = GetAdditionalLightsCount();
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPosition);
-        half3 attenuatedLightColor = light.color * (light.distanceAttenuation * light.shadowAttenuation);
+        half3 attenuatedLightColor = light.color * ((light.distanceAttenuation * light.shadowAttenuation));
+        // diffuseColor += attenuatedLightColor;
         diffuseColor += LightingLambert(attenuatedLightColor, light.direction, WorldNormal);
         specularColor += LightingSpecular(attenuatedLightColor, light.direction, WorldNormal, WorldView, White, Smoothness);
     }
