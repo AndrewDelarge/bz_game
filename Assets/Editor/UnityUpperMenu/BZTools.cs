5fef84826dfd1c5d1214b512ea2ffbfe69d2d765
Andrey Erofeev <a.erofeev@nexters.com>
2023-02-15 16:45:55 +0200

    [dev tools]
    Удаление рут сцене после выхода из плеймода

--- Assets/Editor/UnityUpperMenu/BZTools.cs
+++ Assets/Editor/UnityUpperMenu/BZTools.cs
@@ -1,16 +1,23 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Editor.UnityUpperMenu
 {
     [ExecuteInEditMode]
+    [InitializeOnLoad]
     public class BZTools : MonoBehaviour
     {
         public const string ROOT_SCENE_PATH = "Assets/Scenes/_root.unity";
-        private static Scene _rootScene;
-
+        
+        private static readonly Action _exitPlaymode;
+        
+        static BZTools() {
+            EditorApplication.playModeStateChanged += ChangePlayModeHandler;
+            _exitPlaymode += ClearRootScene;
+        }
+        
         [MenuItem("BZ Tools/Play current scene")]
         static void StartCurrentScene()
         {
@@ -18,6 +25,24 @@ namespace Editor.UnityUpperMenu
 
             var rootScene = EditorSceneManager.OpenScene(ROOT_SCENE_PATH, OpenSceneMode.Additive);
             EditorSceneManager.SetActiveScene(rootScene);
+        }
+
+        private static void ChangePlayModeHandler(PlayModeStateChange obj) {
+            if (obj == PlayModeStateChange.EnteredEditMode ) 
+            {
+                _exitPlaymode.Invoke();
+            }
+
+            // "mb later" feature  
+            // if (obj == PlayModeStateChange.ExitingEditMode) {
+            //     _rootScene = EditorSceneManager.OpenScene(ROOT_SCENE_PATH, OpenSceneMode.Additive);
+            //     EditorSceneManager.SetActiveScene(_rootScene);
+            // }
+        }
+        
+        private static void ClearRootScene() {
+            var scene = EditorSceneManager.GetSceneByPath(ROOT_SCENE_PATH);
+            EditorSceneManager.CloseScene(scene, removeScene:true);
         }
     }
 }
