4be0ecced756acc668d9c3f4513d92ca49bd9eaf
Andrey Erofeev <a.erofeev@nexters.com>
2023-07-27 15:41:25 +0300

    [code] фикс смерти на бегу

--- Assets/Source/Gameplay/Characters/Player/States/PlayerDeadState.cs
+++ Assets/Source/Gameplay/Characters/Player/States/PlayerDeadState.cs
@@ -1,17 +1,18 @@
 ﻿using game.core.Storage.Data.Character;
+using game.Source.core.Common;
 
 namespace game.Gameplay.Characters.Player
 {
     public class PlayerDeadState : PlayerStateBase<CharacterStateEnum, PlayerCharacterContext>
     {
-        private float _deathLenght;
         public override bool CheckExitCondition() => false;
 
         public override void Enter() {
+            context.animation.SetMotionVelocityPercent(0, true);
             context.animation.PlayAnimation(CharacterAnimationEnum.DEATH);
             context.animation.onAnimationComplete.Add(OnAnimationComplete);
         }
-
+        
         private void OnAnimationComplete(CharacterAnimationEnum anim) {
             if (anim == CharacterAnimationEnum.DEATH) {
                 context.animation.Disable();
